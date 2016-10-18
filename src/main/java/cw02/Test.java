94eda5696036561298a379e882818e68b17db5d5
mankunianeth <deyneko55@gmail.com>
2016-10-18 18:31:16 +0300

    Add hw01.

---
+++ src/main/java/cw02/Test.java
@@ -0,0 +1,37 @@
+package cw02;
+
+public class Test {
+    public static void main(String[] args) {
+        boolean b1 = true;
+        boolean b2 = false;
+
+        char c1 = 54;
+        char c2 = 't';
+        char c3 = '\ufaab';
+
+        byte b3 = 12; // -128 ... 127
+
+        short s1 = 32767; //
+
+        int i1 = 2_000_000_000;
+
+        long l1 = 9_000_000_000_000_000_000L;
+
+        float f1 = 10.0F / 3;
+
+        double d1 = 10.0 / 3;
+
+        System.out.println(f1);
+        // System.out.println(d1);
+
+        System.out.println(20 / 10);
+        System.out.println(20 * 10);
+        System.out.println(20 - 10);
+        System.out.println(20 + 10);
+
+        int a = 10;
+        int b = 20;
+        int c;
+        a = a / b;
+    }
+}
