213de1a0971cf6d357e24c42376f84b18d1b0ea8
Eugene1992 <deyneko55@gmail.com>
2016-10-21 18:39:46 +0300

    World11

--- sasha/src/main/java/HomeWork046.java
+++ sasha/src/main/java/HomeWork046.java
@@ -7,8 +7,52 @@
 Пользователь выбирает один из предложенных вариантов, программа выполняет соответствующий перевод числа и выводит
 на экран результат.*/
 
+import java.util.Scanner;
+
 public class HomeWork046 {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter some number:");
+        int num = scan.nextInt();
+        System.out.println("Choose your number system:\n1) 2\n2) 8\n3) 16");
+        int numSys = scan.nextInt();
+        String result = "";
+        switch (numSys) {
+            case 2:
+                while (num >= 1) {
+                    result = num % 2 + result;
+                    num = num / 2;
+                }
+                System.out.println(result);
+                break;
 
+            case 8:
+                while (num > 8) {
+                    result = num % 8 + result;
+                    num = num / 8;
+                }
+                result = num + result;
+                System.out.println(result);
+                break;
+
+            case 16:
+                int cur = 0;
+                while (num > 0) {
+                    cur = num % 16;
+                    if (cur >= 10) {
+                        if (cur == 10) result = "A" + result;
+                        if (cur == 11) result = "B" + result;
+                        if (cur == 12) result = "C" + result;
+                        if (cur == 13) result = "D" + result;
+                        if (cur == 14) result = "E" + result;
+                        if (cur == 15) result = "F" + result;
+                    } else {
+                        result = num + result;
+                    }
+                    num = num / 16;
+                }
+                System.out.println(result);
+                break;
+        }
     }
 }
